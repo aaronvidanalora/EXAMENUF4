3b08aa0148c7e3292a903e7e42f662ee0df3036f
aaronvidanalora <120584227+aaronvidanalora@users.noreply.github.com>
2024-05-30 20:53:59 +0000

    router header puesto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap aquí
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <GlobalProvider> */}
+      <App />
+    {/* </GlobalProvider> */}
   </React.StrictMode>,
-)
+);
